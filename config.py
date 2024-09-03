116f2a794df93efccabe87057495f71fcdfc580f
jan <xpanj19@gmail.com>
2024-09-03 11:16:17 +0200

    minor change

---
+++ config.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Configuration details
+hostname = st.secrets["api"]["hostname"]
+token = st.secrets["api"]["token"]
+workspace_id = "gdc_demo_a3da0f24-9c32-4f19-9848-43dfe4096416"
+data_source_id = "gdc_demo_20557729-8b95-4121-b942-6abb88b735ad"
